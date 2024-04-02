37732d5b6d8db61987bfcc91693e643bb09c7095
martin.novak <martin.novak@notino.com>
2024-04-02 20:27:22 +0200

    Add description on new columns

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ with col1:
 with col2:
     st.title('Martin Novak')
     content = """
-    Hello, I am Martin and I am a web analytic at Notino.
+    Hello, I am Martin and I am a web analyst at Notino.
     """
     st.write(content)
 
+content2 = """
+Below you can find some apps I built in Python. Feel free to contact me!
+"""
+
+st.write(content2)
